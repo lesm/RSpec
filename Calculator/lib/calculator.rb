e965187c9c0b5cb7f2c4ebe720f61af6162d09f8
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-01-30 03:49:15 -0600

    Passing test #add

--- Calculator/lib/calculator.rb
+++ Calculator/lib/calculator.rb
@@ -1,3 +1,7 @@
 class Calculator
 
+  def add num_uno, num_dos
+    num_uno + num_dos
+  end
+  
 end
--- Calculator/spec/calculator_spec.rb
+++ Calculator/spec/calculator_spec.rb
@@ -1,5 +1,9 @@
 require 'calculator'
 
 describe Calculator do
-
+  context '#add' do
+    it 'return 4 when receives 2,2' do
+      expect(Calculator.new.add(2,2)).to eq 4 
+    end
+  end
 end
