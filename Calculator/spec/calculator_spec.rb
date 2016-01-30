160be2814c58356865186a3d293911321b3e4d0b
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-01-30 03:59:41 -0600

    Refactorig test

--- Calculator/spec/calculator_spec.rb
+++ Calculator/spec/calculator_spec.rb
@@ -1,14 +1,19 @@
 require 'calculator'
 
 describe Calculator do
+
+  before { @cal = Calculator.new }
+
   context '#add' do
     it 'return 4 when receives 2,2' do
-      expect(Calculator.new.add(2,2)).to eq 4 
+      expect(@cal.add(2,2)).to eq 4 
     end
   end
+  
   context '#subtract' do
     it 'return 2 when receives 6,4' do
-      expect(Calculator.new.subtract(6,4)).to eq 2
+      expect(@cal.subtract(6,4)).to eq 2
     end
   end
+
 end
